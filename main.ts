899d73b9a02d5de10d63b91f0fb3b61f6eaaf837
MKKing6 <69699405+MKKing6@users.noreply.github.com>
2020-10-03 16:36:03 -0400

    Added a few controls, need to add more

--- main.ts
+++ main.ts
@@ -1,3 +1,31 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setVelocity(0, -20)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mysprite2 == 0) {
+        mySprite.setVelocity(20, 0)
+    }
+})
+controller.down.onEvent(ControllerButtonEvent.Released, function () {
+    mySprite.setVelocity(0, 0)
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    mySprite.setVelocity(0, 0)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setVelocity(20, 0)
+})
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    mySprite.setVelocity(0, 0)
+})
+controller.up.onEvent(ControllerButtonEvent.Released, function () {
+    mySprite.setVelocity(0, 0)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setVelocity(0, 20)
+})
+let mysprite2 = 0
+let mySprite: Sprite = null
 tiles.setTilemap(tiles.createTilemap(hex`1000100003030303030303030303030303030303010101010101010101010101010303030101010101010101010101010101030303030303030303030303030101010103030303030303030303030303010101030301010301010101030303030101010301010103010101010103030301010103010101010103010101010303010101030101030101030303010101010101010301010303030303030301010101010103010103030303030303030101010103030301010303030303030303030303030303010101010303030303030303030303030301010101010101010101010101020303030101010101010101010101010203030303030303030303030303030303`, img`
     2 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
     . . . . . . . . . . . . . 2 . . 
@@ -16,7 +44,7 @@ tiles.setTilemap(tiles.createTilemap(hex`100010000303030303030303030303030303030
     2 . 2 . . . . . . . . . . . . 2 
     . . . 2 2 2 2 2 2 2 2 2 2 2 2 2 
     `, [myTiles.transparency16,sprites.castle.tilePath5,myTiles.tile1,sprites.castle.tileGrass1], TileScale.Sixteen))
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -34,4 +62,8 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+mysprite2 = 0
 mySprite.setPosition(10, 21)
+forever(function () {
+    scene.cameraFollowSprite(mySprite)
+})
